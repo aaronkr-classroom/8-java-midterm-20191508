8b3830c7285e66c969d4c20ecdb525d3446dd301
20191508 <164975608+20191508@users.noreply.github.com>
2024-06-18 15:04:35 +0900

    Add files via upload

---
+++ 8-java-midterm-20191508/Main.java
@@ -0,0 +1,21 @@
+private String isbn;
+private String title;
+private String author;
+private int year;
+private boolean available;
+
+public LibraryBook(Book book) {
+    // 생성자
+}
+
+public LibraryBook(String isbn, String title, String author, int year) {
+    // 생성자
+}
+
+@Override
+public String getIsbn(); // return isbn
+public String getTitle(); // return title
+public String getAuthor(); // return author
+public int getYear(); // return year
+public boolean isAvailable(); // return available
+public void setAvailable(boolean available);
